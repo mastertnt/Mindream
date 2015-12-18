0d6e4fb81639f95d46c01d33175280945ed0cbd9
nby <nby.comptes@outlook.com>
2015-12-18 08:39:35 +0100

    [BLD] Remove externals binaries

---
+++ ThirdParty/XRay/XGraph/Controls/Connection.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using PropertyChanged;
+using XGraph.Extensions;
+using XGraph.ViewModels;
+
+namespace XGraph.Controls
+{
+    /// <summary>
+    /// This class represents a connection.
+    /// </summary>
+    /// <!-- Nicolas Baudrey -->
+    [ImplementPropertyChanged]
+    public class Connection : ContentControl
+    {
+        #region Fields
+
+        /// <summary>
+        /// This field stores the geometry to draw.
+        /// </summary>
+        private PathGeometry mDrawingGeometry;
+
+        /// <summary>
+        /// This field stores the pen of the adorner.
+        /// </summary>
+        private readonly Pen mPen;
+
+        #endregion // Fields.
+
+        #region Constructors
+
+        /// <summary>
+        /// Static constructor.
+        /// </summary>
+        static Connection()
+        {
+            // set the key to reference the style for this control
+            FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(typeof(Connection), new FrameworkPropertyMetadata(typeof(Connection)));
+        }
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public Connection()
+        {
+            this.mPen = new Pen(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#4A7EBB")), 1);
+            this.mPen.LineJoin = PenLineJoin.Round;
+        }
+
+        #endregion // Constructors.
+
+        #region Methods
+
+        /// <summary>
+        /// This method is called to render the adorner.
+        /// </summary>
+        /// <param name="pDC">The current drawing context.</param>
+        protected override void OnRender(DrawingContext pDC)
+        {
+            base.OnRender(pDC);
+            pDC.DrawGeometry(null, this.mPen, this.mDrawingGeometry);
+
+            // without a background the OnMouseMove event would not be fired
+            // Alternative: implement a Canvas as a child of this adorner, like
+            // the ConnectionAdorner does.
+            pDC.DrawRectangle(Brushes.Transparent, null, new Rect(RenderSize));
+        }
+
+        /// <summary>
+        /// Method called when the control content changed.
+        /// </summary>
+        /// <param name="pOldContent">The previous content.</param>
+        /// <param name="pNewContent">The new content.</param>
+        protected override void OnContentChanged(object pOldContent, object pNewContent)
+        {
+            ConnectionViewModel lPreviousContent = this.Content as ConnectionViewModel;
+            if (lPreviousContent != null)
+            {
+                lPreviousContent.Output.PropertyChanged -= this.OnPortPropertyChanged;
+                lPreviousContent.Input.PropertyChanged -= this.OnPortPropertyChanged;
+            }
+
+            base.OnContentChanged(pOldContent, pNewContent);
+
+            // The content is the view model.
+            ConnectionViewModel lNewContent = pNewContent as ConnectionViewModel;
+            if (lNewContent != null)
+            {
+                lNewContent.Output.PropertyChanged += this.OnPortPropertyChanged;
+                lNewContent.Input.PropertyChanged += this.OnPortPropertyChanged;
+                this.UpdatePathGeometry();
+            }
+        }
+
+        /// <summary>
+        /// Called when [port property changed].
+        /// </summary>
+        /// <param name="pSender">The sender.</param>
+        /// <param name="pEventArgs">The <see cref="System.ComponentModel.PropertyChangedEventArgs"/> instance containing the event data.</param>
+        private void OnPortPropertyChanged(object pSender, System.ComponentModel.PropertyChangedEventArgs pEventArgs)
+        {
+            this.UpdatePathGeometry();
+        }
+
+        /// <summary>
+        /// This method updates the final geometry for the path.
+        /// </summary>
+        /// <returns>The path geometry.</returns>
+        private void UpdatePathGeometry()
+        {
+            ConnectionViewModel lViewModel = this.Content as ConnectionViewModel;
+            if
+                (lViewModel != null)
+            {
+                if
+                    (this.mDrawingGeometry == null)
+                {
+                    this.mDrawingGeometry = new PathGeometry();
+                }
+                List<Point> lPoints = lViewModel.Output.Position.GetShortestPath(lViewModel.Input.Position);
+                this.mDrawingGeometry.Figures.Clear();
+                PathFigure lFigure = new PathFigure {StartPoint = lViewModel.Output.Position};
+                lPoints.RemoveAt(0);
+                lFigure.Segments.Add(new BezierSegment(lPoints[0], lPoints[1], lPoints[2], true));
+                this.mDrawingGeometry.Figures.Add(lFigure);
+            }
+        }
+
+        #endregion // Methods.
+    }
+}
---
+++ ThirdParty/XRay/XGraph/Controls/NodeView.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using XGraph.ViewModels;
+
+namespace XGraph.Controls
+{
+    /// <summary>
+    /// Class defining a node in the graph view.
+    /// </summary>
+    [TemplatePart(Name = PART_PORT_CONTAINER, Type = typeof(PortContainer))]
+    public class NodeView : ContentControl
+    {
+        #region Fields
+
+        /// <summary>
+        /// Name of the parts that have to be in the control template.
+        /// </summary>
+        private const string PART_PORT_CONTAINER = "PART_PortContainer";
+
+        /// <summary>
+        /// The inner port container
+        /// </summary>
+        private PortContainer mInnerPortContainer;
+
+        #endregion // Fields.
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes the <see cref="NodeView"/> class.
+        /// </summary>
+        static NodeView()
+        {
+            FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(typeof(NodeView), new FrameworkPropertyMetadata(typeof(NodeView)));
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NodeView"/> class.
+        /// </summary>
+        public NodeView()
+        {
+            
+        }
+
+        #endregion // Constructors.
+
+        #region Methods
+
+        /// <summary>
+        /// Method called when the control content changed.
+        /// </summary>
+        /// <param name="pOldContent">The previous content.</param>
+        /// <param name="pNewContent">The new content.</param>
+        protected override void OnContentChanged(object pOldContent, object pNewContent)
+        {
+            base.OnContentChanged(pOldContent, pNewContent);
+
+            BindingOperations.ClearAllBindings(this);
+
+            // The content is the view model.
+            NodeViewModel lNewContent = pNewContent as NodeViewModel;
+            if (lNewContent != null)
+            {
+                // Setting the content data template.
+                this.ContentTemplate = lNewContent.DataTemplate;
+            }
+        }
+
+        /// <summary>
+        /// Method called when the control template is applied.
+        /// </summary>
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            // Getting the parts of the control.
+            this.mInnerPortContainer = this.GetTemplateChild(PART_PORT_CONTAINER) as PortContainer;
+
+            if (this.mInnerPortContainer == null)
+            {
+                throw new Exception("NodeView control template not correctly defined.");
+            }
+
+            // Binding the Ports property.
+            Binding lNodesBinding = new Binding("Ports");
+            lNodesBinding.Source = this.Content;
+            lNodesBinding.Mode = BindingMode.OneWay;
+            this.mInnerPortContainer.SetBinding(ItemsControl.ItemsSourceProperty, lNodesBinding);
+        }
+
+        #endregion // Methods.
+    }
+}
